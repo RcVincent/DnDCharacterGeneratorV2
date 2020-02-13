3b86c37bf1943b85f52ba6f3fc80462b9964ab6f
Ryan <rvincent@ycp.edu>
2020-02-13 18:38:47 -0500

    Add project files.

---
+++ DnDCharacterGeneratorV2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DnDCharacterGeneratorV2.MvcApplication" Language="C#" %>
---
+++ DnDCharacterGeneratorV2/Models/Character.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DnDCharacterGeneratorV2.Models
+{
+    public enum Race
+    {
+        Human,
+        Elf,
+        Dwarf,
+        Halfling,
+        Gnome,
+        Tiefling,
+        DragonBorn,
+        HalfOrk,
+        HalfElf
+    }
+    public enum Skills { 
+        Acrobatics,
+        AnimalHandling,
+        Arcana,
+        Athletics,
+        Deception,
+        History,
+        Insight,
+        Intimidation,
+        Investigation,
+        Medicine,
+        Nature,
+        Perception,
+        Performance,
+        Persuasion,
+        Religion,
+        SleightOfHand,
+        Stealth,
+        Survival
+    }
+
+    public class Character
+    {
+        //core character data
+        public int HP { get; set; }
+        public int AC { get; set; }
+        public string Name { get; set; }
+        public int Proficiency { get; set; }
+
+        //parameter data
+        public int Strength { get; set; }
+        public int Dexterity { get; set; }
+        public int Constitution { get; set; }
+        public int Wisdom { get; set; }
+        public int Charisma { get; set; }
+        public int Intelligence { get; set; }
+
+    }
+}
